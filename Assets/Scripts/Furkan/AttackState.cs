fdf354b9a1c136b1eee9ba5b0d41fbbf5f222ce7
Anot-dev <anotprivate4@gmail.com>
2025-08-25 13:07:57 +0300

    düşman state machine kuruyorum

---
+++ Assets/Scripts/Furkan/AttackState.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.Android;
+
+public class AttackState : StateMachineBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform playerTransform;
+    private NavMeshAgent agent;
+    private ChaseState chaseState;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponent<NavMeshAgent>();
+        chaseState = animator.GetBehaviour<ChaseState>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 distance = playerTransform.position - animator.transform.position;
+        if(distance.magnitude > chaseState.attackDistance)
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
